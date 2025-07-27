5ec32c809d62304757b94c84a69729a2cfda74a4
L10NH34RT <zleo050907@gmail.com>
2025-07-27 11:16:27 -0700

    Update Quiz 3 Code.asm

--- Quiz 3 contents/Quiz 3 Code.asm
+++ Quiz 3 contents/Quiz 3 Code.asm
@@ -1,1 +1,34 @@
+section .data
+    x dd 10
+    y dd 25
+    z dd 15
 
+section .bss
+    result resd 1
+
+section .text
+    global _start
+
+add_three_numbers:
+    push ebp
+    mov ebp, esp
+    
+    add eax, ebx
+    add eax, ecx
+    
+    mov esp, ebp
+    pop ebp
+    ret
+
+_start:
+    mov eax, [x]
+    mov ebx, [y]
+    mov ecx, [z]
+    
+    call add_three_numbers
+    
+    mov [result], eax
+    
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
